7e17b13743086baf10677ef1c78c2b0bbaacf194
Marcos Vinicius Sombra <markos.vs@live.com>
2017-03-21 20:02:32 -0300

    Update Cadeira.java

--- Cadeira.java
+++ Cadeira.java
@@ -2,24 +2,45 @@ package peoo5;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author marcos-sombra
- */
 public class Cadeira {
     //atributos
+    private String nome;
+    private String professor;
     private ArrayList<Aluno> alunos;
+    //contrutor
+    public Cadeira(String cadeira){
+        this.nome = cadeira;
+    }
     //getters
     public ArrayList<Aluno> getAlunos() {
         return this.alunos;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+    
     //setters
     public void setAlunos(ArrayList<Aluno> alunos) {
         this.alunos = alunos;
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+    
     //métodos
     public void cadastrarAluno(Aluno a){
         getAlunos().add(a);
+        a.getCadeiras().add(new Pair(this, "Cursando"));
     }
     
 }
