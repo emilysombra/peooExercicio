ce5825884e9e6b3e47e8b3133fdc422d589c8739
Marcos Vinicius Sombra <markos.vs@live.com>
2017-03-21 20:02:01 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -2,33 +2,49 @@ package peoo5;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author marcos-sombra
- */
 public class Aluno {
     //atributos
     private String nome;
     private final long matricula;
     private int semestre;
-    private ArrayList<Cadeira> cadeiras;
+    private Curso curso;
+    private ArrayList<Pair<Cadeira, String>> cadeiras;
+    private String situacaoCurso;
     //construtores
     public Aluno(long m) {
         this.matricula = m;
         this.semestre = 1;
+        this.situacaoCurso = "Graduando";
     }
     public Aluno(long m, String nome) {
         this.matricula = m;
         this.semestre = 1;
         this.nome = nome;
+        this.situacaoCurso = "Graduando";
+    }
+    public Aluno(long m, String nome, Curso c) {
+        this.matricula = m;
+        this.semestre = 1;
+        this.nome = nome;
+        this.situacaoCurso = "Graduando";
+        this.curso = c;
+        //getCurso().getAlunos().add(this);
     }
     //getters    
     public long getMatricula(){
         return this.matricula;
     }
+    
+    public Curso getCurso(){
+        return this.curso;
+    }
+    
+    public String getNomeCurso(){
+        return this.curso.getNome();
+    }
 
-    public ArrayList<Cadeira> getCadeiras() {
-        return cadeiras;
+    public ArrayList<Pair<Cadeira, String>> getCadeiras() {
+        return this.cadeiras;
     }
     
     public String getNome(){
@@ -36,12 +52,77 @@ public class Aluno {
     }
 
     public int getSemestre() {
-        return semestre;
+        return this.semestre;
+    }
+
+    private String getSituacaoCurso() {
+        return situacaoCurso;
     }
     
     //setters
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    private void setSituacao(String sit){
+        this.situacaoCurso = sit;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public void setCadeiras(ArrayList<Pair<Cadeira, String>> cadeiras) {
+        this.cadeiras = cadeiras;
+    }
+    
+    //métodos
+    public void concluirCurso(){
+        if(numCadeirasCursadas() == curso.getCadeiras().size())
+            setSituacao("Graduado");
+        else
+            setSituacao("Desistente");
+    }
+    public void avancarSemestre(){
+        this.semestre++;
+    }
+    public int numCadeirasCursadas(){
+        int cont = 0;
+        for (Pair<Cadeira, String> cadeira : this.cadeiras) {
+            if("Cursada".equals(cadeira.getSecond()))
+                cont++;
+        }
+        return cont;
+    }
+    public int cadeirasParaConcluir(){
+        return this.curso.getCadeiras().size() - numCadeirasCursadas();
+    }
+    public void matricularCadeira(Cadeira c){
+        for (Cadeira cadeira : this.curso.getCadeiras()) 
+            if(c.equals(cadeira)){
+                c.getAlunos().add(this);
+                getCadeiras().add(new Pair(c, "Cursando"));
+            }   
+    }
+    public void concluirCadeira(Cadeira c){
+        for(Pair<Cadeira, String> cadeira : this.cadeiras)
+            if(cadeira.getFirst().equals(c))
+                cadeira.setSecond("Cursada");
+        
+    }
+    public void desistirCadeira(Cadeira c){
+        for(Pair<Cadeira, String> cadeira : this.cadeiras)
+            if(cadeira.getFirst().equals(c))
+                cadeira.setSecond("Desistente");
+        
+    }
+    public String situacaoCurso(){
+        return getSituacaoCurso();
+    }
+    public String situacaoCadeira(Cadeira c){
+        for(Pair<Cadeira, String> cadeira : this.cadeiras)
+            if(cadeira.getFirst().equals(c))
+                return cadeira.getSecond();
+        return null;
     }
     
     
